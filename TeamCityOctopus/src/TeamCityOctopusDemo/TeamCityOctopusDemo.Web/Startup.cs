2db0ad289f470a223d17da7cf92e8875b8d5c92b
Matt Ash <matthew.a.ash@gmail.com>
2015-07-21 13:44:39 -0500

    creating initial VS solution
    Created web app, class library, and test project

---
+++ TeamCityOctopus/src/TeamCityOctopusDemo/TeamCityOctopusDemo.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamCityOctopusDemo.Web.Startup))]
+namespace TeamCityOctopusDemo.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
