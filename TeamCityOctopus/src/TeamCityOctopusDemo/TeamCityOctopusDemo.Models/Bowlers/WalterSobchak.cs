8d8148cbebede6ac287a722707f8057469cab0f9
Matt Ash <matthew.a.ash@gmail.com>
2015-07-28 12:12:12 -0500

    reset tests to initial state

--- TeamCityOctopus/src/TeamCityOctopusDemo/TeamCityOctopusDemo.Models/Bowlers/WalterSobchak.cs
+++ TeamCityOctopus/src/TeamCityOctopusDemo/TeamCityOctopusDemo.Models/Bowlers/WalterSobchak.cs
@@ -15,8 +15,6 @@ namespace TeamCityOctopusDemo.Models.Bowlers
 
 		public bool ShouldIRoll( DateTime date )
 		{
-			return IsShabbos( date );
-
 			return !IsShabbos( date );
 
 			//if ( IsShabbos( date ) )
