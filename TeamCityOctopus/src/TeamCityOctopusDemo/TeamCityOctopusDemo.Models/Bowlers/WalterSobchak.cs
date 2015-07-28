9c2a45c317fab2417bf5de246b34f161af7451d2
Matt Ash <matthew.a.ash@gmail.com>
2015-07-28 12:17:21 -0500

    testing vcs trigger

--- TeamCityOctopus/src/TeamCityOctopusDemo/TeamCityOctopusDemo.Models/Bowlers/WalterSobchak.cs
+++ TeamCityOctopus/src/TeamCityOctopusDemo/TeamCityOctopusDemo.Models/Bowlers/WalterSobchak.cs
@@ -15,11 +15,11 @@ namespace TeamCityOctopusDemo.Models.Bowlers
 
 		public bool ShouldIRoll( DateTime date )
 		{
-			return !IsShabbos( date );
+			//return !IsShabbos( date );
 
-			//if ( IsShabbos( date ) )
-			//	throw new ShomerShabbosException();
-			//return true;
+			if ( IsShabbos( date ) )
+				throw new ShomerShabbosException();
+			return true;
 		}
 
 		#endregion
