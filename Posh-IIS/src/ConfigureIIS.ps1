7c7015e4679a8cb50fd173be350e65fb4814d974
ashmatthew <matthew.a.ash@gmail.com>
2017-07-02 15:50:27 -0500

    automating host record addition if not already found

--- Posh-IIS/src/ConfigureIIS.ps1
+++ Posh-IIS/src/ConfigureIIS.ps1
@@ -1,5 +1,5 @@
-$ApplicationPoolName = ""
-$Hostname = ""
+$applicationPoolName = ""
+$hostName = "ICCsite.local"
 
 Function CreateApplicationPool {
 
@@ -10,7 +10,16 @@ Function CreateWebsite {
 }
 
 Function BindToHostname {
-    #append entry to hosts
+    
+    
+    #append entry to hosts - NOT related to WebAdministration
+    $hostPath = "C:\Windows\System32\drivers\etc\hosts"
+    if(Select-String -path $hostPath -Pattern $hostName) {
+        Write-host "Found host entry for $hostName"
+    } else {
+        Write-host "Adding host record found for $hostName"
+        Add-content $hostPath "`r127.0.0.1`t`t`t$hostName"
+    }
 }
 
 Function CreateApplication {
